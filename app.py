153d838c51e2e8943cf3ab8e36679f866b3adf4c
SpOoKyKala <51444095+SpOoKyKala@users.noreply.github.com>
2026-05-14 11:21:08 +0500

    под телефоны

--- app.py
+++ app.py
@@ -474,4 +474,5 @@ def upload_base64():
 if __name__ == '__main__':
     import os
     os.makedirs('static/uploads/lessons', exist_ok=True)
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
